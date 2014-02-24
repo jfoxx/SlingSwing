703609d73a780dae500f24baeb90f503cd4ac100
jack <jack.enqvist@gmail.com>
2014-02-24 10:16:39 +0100

    refactoring in grapple script to keep the update cleaner.

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -22,31 +22,51 @@ public class Grapple : MonoBehaviour {
 	}
 
 	void Start () {
-		targetPosition = transform.position;
-		player = GameObject.FindGameObjectWithTag("Player");
-		if(player == null){
-			Debug.LogError("Player not found!!");
-		}
-		energyField = transform.FindChild("EnergyField");
+		targetPosition 	= transform.position;
+		energyField 	= transform.FindChild("EnergyField");
+		player 			= GameObject.FindGameObjectWithTag("Player");
+
 	}
 
 
 	void Update () {
+
 		if(player == null){return;}
 
-		if(Input.touchCount > 0){
+		ShootGrappleTouch();
 
+		ShootGrappleMouse();
+
+
+		if(touchExit || Input.GetMouseButtonUp(0))
+		{
+			hitSet = false;
+		}
+		
+		energyField.gameObject.SetActive( hitSet );
+
+		transform.position = targetPosition;
+
+	}
+
+	void ShootGrappleTouch()
+	{
+		if(Input.touchCount > 0)
+		{
+			
 			if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0)) {
-
-				Vector3 touchPos = new Vector3( Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y, 0 );
-
+				
+				Vector3 touchPos = new Vector3(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, 
+				                               Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y, 
+				                               0 );
+				
 				RaycastHit2D hit = Physics2D.Raycast (player.transform.position, (touchPos - player.transform.position).normalized, raycastDistance );
 				
-				if (hit != null && hit.transform != null) 
+				if ( hit.transform != null ) 
 				{
 					Debug.Log ("grabbed " + hit.transform.name);
 					targetPosition = hit.point;
-
+					
 					transform.parent = hit.transform;
 					hitSet = true;
 					player.transform.SendMessage("OnHitSet");
@@ -54,20 +74,19 @@ public class Grapple : MonoBehaviour {
 				} else {
 					player.transform.SendMessage("OnHitExit");
 					hitSet = false;
-					//its a miss
 				}
-
-				//targetPosition = touchPos;
-				myguitext = "toched at: " + touchPos;
-			
 			}
 		}
+	}
 
-		// debugging in computer	
+	void ShootGrappleMouse()
+	{
 		if (Input.GetMouseButtonDown(0)) 
 		{
 			
-			Vector3 touchPos = new Vector3( Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0 );
+			Vector3 touchPos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+			                               Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+			                               0 );
 			
 			RaycastHit2D hit = Physics2D.Raycast (player.transform.position, (touchPos - player.transform.position).normalized, raycastDistance);
 			
@@ -84,25 +103,6 @@ public class Grapple : MonoBehaviour {
 				player.transform.SendMessage("OnHitExit");
 				Debug.Log ("its a miss ");
 			}
-			
-			//targetPosition = touchPos;
-			myguitext = "toched at: " + touchPos;
-			
 		}
-
-		if(touchExit || Input.GetMouseButtonUp(0)){
-			hitSet = false;
-		}
-		
-		energyField.gameObject.SetActive( hitSet);
-
-
-		transform.position = targetPosition;
-
-	}
-
-	void OnGUI(){
-		//GUILayout.Label(myguitext);
-		//GUILayout.Label("hitSet = " + hitSet);
 	}
 }
